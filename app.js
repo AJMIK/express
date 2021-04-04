8c42e5d970d4af8426ae3bec8387752cdbbf5b5f
AJMIK <ajmimkabeer24@gmail.com>
2021-04-04 13:36:13 +0530

    compleated

---
+++ app.js
@@ -0,0 +1,13 @@
+var express=require('express')
+var app=express()
+
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+app.get('/home',(req,res)=>{
+    res.send("welcome to home page")
+})
+app.listen(3000,()=>{
+    console.log("server started at http://localhost:3000/")
+})
